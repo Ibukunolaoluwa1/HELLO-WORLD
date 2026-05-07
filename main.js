e31b6954e8dd8be6d330634f020983078b3e6e05
Ibukunoluwa1 <olaflorence2310@gmail.com>
2026-05-07 20:58:13 +0100

    Database Practice for fun

---
+++ main.js
@@ -0,0 +1,7 @@
+const welcome = require("./message");
+
+const {multiply, divide} = require("./calculator")
+
+welcome()
+console.log(multiply(3, 4))
+console.log(divide(25, 5))
---
+++ message.js
@@ -0,0 +1,5 @@
+function welcome() {
+    console.log("Welcome to this class Florence!");
+}
+
+module.exports = welcome
